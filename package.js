d049bf6de13d0e26225e70c89b6b4c4f239e5f15
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-14 16:20:05 +0530

    package restructured added tests

--- package.js
+++ package.js
@@ -6,5 +6,11 @@ Npm.depends({"redis" : "0.8.3"});
 
 Package.on_use(function (api, where) {
   api.use(['mongo-livedata'], 'server');
-  api.add_files(['cluster.js'], 'server');
+  api.add_files(['lib/redis.js', 'lib/cluster.js'], 'server');
+});
+
+Package.on_test(function (api) {
+  api.use(['mongo-livedata'], 'server');
+  api.add_files(['lib/redis.js', 'lib/cluster.js'], 'server');
+  api.add_files(['test/publish.js', 'test/subscribe.js'], 'server');
 });
---
+++ test/subscribe.js
@@ -0,0 +1,44 @@
+var Future  = Npm.require('fibers/future');
+
+Tinytest.add('update and subscribe', function(test) {
+
+    var f = new Future();
+    var cluster = new Meteor.Cluster.constructor();
+    var pubClient = Meteor._createRedisClient();
+
+    var collName = Random.id();
+    var syncColl = new Meteor.Collection(collName);
+
+    var orginalUpdate = syncColl.update;
+    var updatingIds = [];
+    syncColl.update = function(query, updateString) {
+        updatingIds.push(query._id);
+        orginalUpdate.call(syncColl, query, updateString);
+        if(updatingIds.length == 2) {
+            f.ret();
+        }
+    };
+
+    syncColl.insert({_id: "1", room: 10});
+    syncColl.insert({_id: "2", room: 10});
+
+    cluster.onsubscribe = function() {
+        f.ret();
+    };
+    cluster.init();
+    cluster.sync(syncColl);
+    f.wait();
+
+    var json = [
+        "server-id",
+        collName,
+        'update',
+        [{room: 10}]
+    ];
+
+    pubClient.publish('meteor', JSON.stringify(json));
+    f = new Future();
+    f.wait();
+    test.equal(updatingIds[0], "1");
+    test.equal(updatingIds[1], "2");
+});
